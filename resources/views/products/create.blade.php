262dabd161e023838835aae59cf57b574a5fceed
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-07-04 12:16:22 +0530

    send data to db

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,14 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
+@extends('layouts.layout')
+@section('title', 'Product Create')
+
+@section('content')
+
+
 
   <h1>Product Create</h1>
+
+  <form method="post" action="{{route('product.store')}}">
+
+    @csrf
+    @method('post')
+
+    <div class="mb-3">
+      <label for="name" class="form-label">Name</label>
+      <input type="text" class="form-control" id="name" name="name">
+    </div>
+
+    <div class="mb-3">
+      <label for="qty" class="form-label">Qty</label>
+      <input type="text" class="form-control" id="qty" name="qty">
+    </div>
+
+    <div class="mb-3">
+      <label for="price" class="form-label">Price</label>
+      <input type="text" class="form-control" id="price" name="price">
+    </div>
+
+    <div class="mb-3">
+      <label for="descrption" class="form-label">Description</label>
+      <input type="text" class="form-control" id="descrption" name="descrption">
+    </div>
+
+
+
+    <input type="submit" class="btn btn-primary" value="Save a new product" />
+  </form>
+
+
   
-</body>
-</html>
+
+
+@endsection
